6d407d36cfa2b0eea2ce8a1320950280b6b4dab7
Loreto <loreto.garde@gmail.com>
2024-10-21 22:09:39 +0200

    refactor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
 function Footer() {
   return (
-
     <footer id="footer">
-      Footer
+      <p>
+        Loreto Garde
+        <a href="https://github.com/loret0g" target="_blank">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/loreto-garde/" target="_blank">
+          <i class="fab fa-linkedin"></i>
+        </a>  
+      </p>
     </footer>
-    
-  )
+  );
 }
 
-export default Footer
+export default Footer;
