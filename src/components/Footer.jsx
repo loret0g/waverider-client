63cbe7ec86f59dc9d3f55cd0123924bb2fc425c7
Loreto <loreto.garde@gmail.com>
2024-10-22 09:38:04 +0200

    first deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
       <p>
         Loreto Garde
         <a href="https://github.com/loret0g" target="_blank">
-          <i class="fab fa-github"></i>
+          <i className="fab fa-github"></i>
         </a>
         <a href="https://www.linkedin.com/in/loreto-garde/" target="_blank">
-          <i class="fab fa-linkedin"></i>
+          <i className="fab fa-linkedin"></i>
         </a>  
       </p>
     </footer>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,26 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-import JetSkiCard from '../components/JetSkiCard'
+import JetSkiCard from "../components/JetSkiCard";
 
 function HomePage() {
-
-  const [jetSki, setJetSki] = useState([])
-  const [sortPrice, setSortPrice] = useState(true)
+  const [jetSki, setJetSki] = useState([]);
+  const [sortPrice, setSortPrice] = useState(true);
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
-  const getData = async() => {
+  const getData = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/jet-ski`)
-      setJetSki(response.data)
+      const response = await axios.get(
+        `${import.meta.env.VITE_SERVER_URL}/api/jet-ski`
+      );
+      setJetSki(response.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const handleSortByPrice = () => {
     const sortedJetSki = [...jetSki].sort((a, b) => {
@@ -30,26 +31,39 @@ function HomePage() {
       }
     });
     setJetSki(sortedJetSki);
-    setSortPrice(!sortPrice);     // Toggle para cambiar el orden
-  };
+    setSortPrice(!sortPrice); // Toggle para cambiar el orden
+  }; 
 
   return (
     <div id="homepage-container">
-      <div className="sort-container">
-        <button onClick={handleSortByPrice} className="sort-button">
-          {sortPrice ? "Ordenar por precio: Descendente" : "Ordenar por precio: Ascendente"}
-        </button>
+      <div className="video">
+        <video autoPlay muted loop>
+          <source src="video/overlay.mp4" type="video/mp4" />
+        </video>
+        <div className="overlay">
+          <a href="#jetski-section" className="scroll-link">Alquila una moto</a>
+        </div>
       </div>
-      
-      <div className="homepage-container container-jetski">
-        {jetSki.length === 0 ? (<p>No disponemos de vehículos en este momento</p>) : (
+
+      <section className="homepage-container container-jetski" id="jetski-section">
+        {jetSki.length === 0 ? (
+          <p>No disponemos de vehículos en este momento</p>
+        ) : (
           jetSki.map((eachJetSki) => (
             <JetSkiCard key={eachJetSki._id} jetSki={eachJetSki} />
           ))
         )}
+      </section>
+
+      <div className="sort-container">
+        <button onClick={handleSortByPrice} className="sort-button">
+          {sortPrice
+            ? "Ordenar por precio: Descendente"
+            : "Ordenar por precio: Ascendente"}
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
