6ef78754c9e9d98a0b3e7ad033a548d653e3a470
Loreto <loreto.garde@gmail.com>
2024-10-24 18:14:36 +0200

    added filter

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
 import JetSkiCard from "../components/JetSkiCard";
+import { Dropdown, DropdownButton } from "react-bootstrap"; // Importar componentes de react-bootstrap
 
 function HomePage() {
   const [jetSki, setJetSki] = useState([]);
   const [sortPrice, setSortPrice] = useState(true);
+  const [sortRating, setSortRating] = useState(null); // Para el filtro de valoraciones
+  const [sortHorsepower, setSortHorsepower] = useState(null); // Para el filtro de potencia
 
   useEffect(() => {
     getData();
@@ -25,15 +27,31 @@ function HomePage() {
 
   const handleSortByPrice = () => {
     const sortedJetSki = [...jetSki].sort((a, b) => {
-      if (sortPrice) {
-        return a.price - b.price; // Ascendente
-      } else {
-        return b.price - a.price; // Descendente
-      }
+      return sortPrice ? a.price - b.price : b.price - a.price;
     });
     setJetSki(sortedJetSki);
     setSortPrice(!sortPrice); // Toggle para cambiar el orden
-  }; 
+  };
+
+  const handleSortByRating = () => {
+    const sortedJetSki = [...jetSki].sort((a, b) => {
+      return sortRating === null || sortRating
+        ? a.averageRating - b.averageRating
+        : b.averageRating - a.averageRating;
+    });
+    setJetSki(sortedJetSki);
+    setSortRating(!sortRating); // Toggle para cambiar el orden
+  };
+
+  const handleSortByHorsepower = () => {
+    const sortedJetSki = [...jetSki].sort((a, b) => {
+      return sortHorsepower === null || sortHorsepower
+        ? a.horsepower - b.horsepower
+        : b.horsepower - a.horsepower;
+    });
+    setJetSki(sortedJetSki);
+    setSortHorsepower(!sortHorsepower); // Toggle para cambiar el orden
+  };
 
   return (
     <div id="homepage-container">
@@ -42,11 +60,48 @@ function HomePage() {
           <source src="video/overlay.mp4" type="video/mp4" />
         </video>
         <div className="overlay">
-          <a href="#jetski-section" className="scroll-link">Alquila una moto</a>
+          <a href="#jetski-section" className="scroll-link">
+            Alquila una moto
+          </a>
         </div>
       </div>
 
-      <section className="homepage-container container-jetski" id="jetski-section">
+      <div className="sort-container">
+        <DropdownButton
+          id="btn-1"
+          className="homepage-dropdown"
+          title="Ordenar por"
+        >
+          <Dropdown.Header>Precio</Dropdown.Header>
+          <Dropdown.Item onClick={() => handleSortByPrice(true)}>
+            Menor precio
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleSortByPrice(false)}>
+            Mayor precio
+          </Dropdown.Item>
+
+          <Dropdown.Header>Potencia</Dropdown.Header>
+          <Dropdown.Item onClick={() => handleSortByHorsepower(true)}>
+            Menor potencia
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleSortByHorsepower(false)}>
+            Mayor potencia
+          </Dropdown.Item>
+
+          <Dropdown.Header>Valoraciones</Dropdown.Header>
+          <Dropdown.Item onClick={() => handleSortByRating(true)}>
+            Mejor valoradas
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleSortByRating(false)}>
+            Peor valoradas
+          </Dropdown.Item>
+        </DropdownButton>
+      </div>
+
+      <section
+        className="homepage-container container-jetski"
+        id="jetski-section"
+      >
         {jetSki.length === 0 ? (
           <p>No disponemos de vehículos en este momento</p>
         ) : (
@@ -55,14 +110,6 @@ function HomePage() {
           ))
         )}
       </section>
-
-      <div className="sort-container">
-        <button onClick={handleSortByPrice} className="sort-button">
-          {sortPrice
-            ? "Ordenar por precio: Descendente"
-            : "Ordenar por precio: Ascendente"}
-        </button>
-      </div>
     </div>
   );
 }
